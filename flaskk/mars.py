dca62318fca10ac076bcf3d7794be6ab268aad7a
Emilia <emilykreydlina@yandex.ru>
2023-02-12 20:53:46 +0300

    Отбор астронавтов задача

--- flaskk/mars.py
+++ flaskk/mars.py
@@ -1,64 +1,117 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def a():
-    return "<h1>Миссия Колонизация Марса</h1>"
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>форма</title>
+                          </head>
+                          <body>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <h1>Анкета претендента 
+                                    на участие в миссии</h1>
+                                    <br>
+                                    <input class="form-control" id="surname" placeholder="Введите фамилию" name="surname">
+                                    <br>
+                                    <input class="form-control" id="name" placeholder="Введите имя" name="name">
+                                    <br>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите ваш email" name="email">
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое у вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="education">
+                                          <option>Начальное образование</option>
+                                          <option>Среднее образование</option>
+                                          <option>Высшее образование</option>
+                                        </select>
+                                     </div>
+                                    <br>
+                                    <label for="form-check">Какие у вас есть профессии?</label>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="first" name="first">
+                                        <label class="form-check-label" for="acceptRules">Инженер-исследователь</label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="second" name="second">
+                                        <label class="form-check-label" for="acceptRules">Инженер-строитель</label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Инженер по жизнеобеспечению</label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="second" name="second">
+                                        <label class="form-check-label" for="acceptRules">Пилот</label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="second" name="second">
+                                        <label class="form-check-label" for="acceptRules">Врач</label>
+                                    </div>
+                                    <div>
+                                        <input type="checkbox" class="form-check-input" id="second" name="second">
+                                        <label class="form-check-label" for="acceptRules">Экзобиолог</label>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="4" name="about"></textarea>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <br>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <br>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <br>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
 
-
-@app.route('/index')
-def index():
-    return "<h1>И на Марсе будут яблони цвести     +!</h1>"
-
-
-@app.route('/promotion')
-def promotion():
-    return '</br>'.join(['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
-                         'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!'])
-
-
-@app.route('/image_mars')
-def image_mars():
-    return """<title>HELLO, Марс!</title>
-    <h1>AAAAAAAAAAAAAAЖди нас, Марс!</h1>
-    <<img src="/static/img/mars.png">
-    """
-
-
-@app.route('/promotion_image')
-def promotion_image():
-    return """<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width">
-    <title>MARS</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link href="static/css/style.css" rel="stylesheet" type="text/css" />
-  </head>
-  <body>
-    <h1>Жди нас, Марс!</h1>
-	<img src="static/img/mars.png">
-      <div class="alert alert-primary" role="alert">
-  Человечество вырастает из детства.
-</div>
-<div class="alert alert-secondary" role="alert">
-  Человечеству мала одна планета.
-</div>
-<div class="alert alert-success" role="alert">
-  Мы сделаем обитаемыми безжизненные пока планеты.
-</div>
-<div class="alert alert-danger" role="alert">
-  И начнем с Марса!
-</div>
-<div class="alert alert-warning" role="alert">
-  Присоединяйся!
-</div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-  </body>
-</html>"""
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(port=8000, host='0.0.0.0')
